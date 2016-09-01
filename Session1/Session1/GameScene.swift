06e266d99f2486573fb35c2c44ed0f819259ff91
qhuydtvt <qhuydtvt@gmail.com>
2016-09-01 20:31:03 +0700

    Cleaned GameScene code

--- Session1/Session1/GameScene.swift
+++ Session1/Session1/GameScene.swift
@@ -13,16 +13,6 @@ class GameScene: SKScene {
     var plane:SKSpriteNode!
     
     override func didMoveToView(view: SKView) {
-//        /* Setup your scene here */
-//        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-//        myLabel.text = "Hello, World!"
-//        myLabel.fontSize = 45
-//        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-//        
-//        self.addChild(myLabel)
-        
-        print("didMoveToView")
-        
         let background = SKSpriteNode(imageNamed: "background.png")
         
         background.anchorPoint = CGPointZero
@@ -37,28 +27,10 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
         if let touch = touches.first {
             let touchPosition = touch.locationInView(self.view)
             plane.position = CGPoint(x: touchPosition.x, y: self.frame.size.height-touchPosition.y)
-        
         }
-//        for touch in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
